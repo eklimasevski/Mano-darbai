0887315839e8850ca793ea80f4e5c89e2c93cb66
Ernestas <ernest.klimasevski@gmail.com>
2023-12-08 12:36:13 +0200

    Complete excercise Reapeting Braking

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int number = Integer.valueOf()
+
+        if (number % 2 == 0){
+            even = even + 1;
+        }else {
+            odd += 1;
+        }
+    }
+}
