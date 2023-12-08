b0d0435afe84d2ca12f679c39f957ced35b26b29
Ernestas <ernest.klimasevski@gmail.com>
2023-12-08 15:35:05 +0200

    Complete: Division

---
+++ Java/part02-Part02_25.Division/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,17 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+        division(3,5);
+
+        // Once you have implemented the division method, you can
+        // try it out here. Fopr example division(3,5);
+        // should print "0.6"
+    }
+
+    // implement the method here
+    public static void division(int numerator, int denominator) {
+        System.out.println( (double) numerator / denominator);
+    }
+}
