e4a4ee51f0d005e00b22b2e5bebc36258388f224
Ernestas <ernest.klimasevski@gmail.com>
2023-12-08 16:26:31 +0200

    Complete: Number Uno & Word

---
+++ Java/part02-Part02_27.NumberUno/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,12 @@
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        System.out.println(numberUno());
+    }
+
+    public static int numberUno() {
+
+        return 1;
+    }
+}
---
+++ Java/part02-Part02_28.Word/part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,13 @@
+
+public class Word {
+
+    public static void main(String[] args) {
+
+        System.out.println(word());
+    }
+
+    public static String word() {
+        return "Hello World!";
+    }
+
+}
