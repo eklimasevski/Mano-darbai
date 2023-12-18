d16fd1943b41a3c67ce1061a6213cbd9850edea0
Ernestas <ernest.klimasevski@gmail.com>
2023-12-18 09:19:32 +0200

    Complete: Agent

---
+++ Java/part04-Part04_13.Agent/part04-Part04_13.Agent/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Agent bond = new Agent("James", "Bond");
+        Agent ionic = new Agent("Ionic", "Bond");
+
+        bond.toString();
+        System.out.println(bond);
+        System.out.println(ionic);
+
+
+    }
+}
