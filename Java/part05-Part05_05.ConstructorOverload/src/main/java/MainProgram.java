5455099a2efa42b3e8b23689a27db7902cde8d69
Ernestas <ernest.klimasevski@gmail.com>
2023-12-19 13:27:54 +0200

    Complete: Constructor Overload

---
+++ Java/part05-Part05_05.ConstructorOverload/src/main/java/MainProgram.java
@@ -0,0 +1,14 @@
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Product tapeMeasure = new Product("tape measure");
+        Product plastger = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plastger);
+        System.out.println(tyre);
+
+    }
+}
