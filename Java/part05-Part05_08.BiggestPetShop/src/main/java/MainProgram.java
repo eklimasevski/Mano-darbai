db178738c3417da87cde5e02a880ce72a99625e4
Ernestas <ernest.klimasevski@gmail.com>
2023-12-20 11:32:25 +0200

    Complete: Biggest pet shop

---
+++ Java/part05-Part05_08.BiggestPetShop/src/main/java/MainProgram.java
@@ -0,0 +1,14 @@
+import javax.crypto.spec.PSource;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+
+
+        Pet lucy = new Pet("Lucy", "golden retriever");
+        Person leo = new Person("Leo", lucy);
+
+        System.out.println(leo);
+    }
+}
+
